8a1b91b5f869d347d7ea6ff19650640e8695064a
PocketCorporation <73258409+PocketCorporation@users.noreply.github.com>
2026-04-08 22:38:52 +0000

    project set up

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontSans = Plus_Jakarta_Sans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight:['300','400','500','600','700']
+});
+
+export const metadata: Metadata = {
+  title: "Patients Kiss",
+  description: "A healthcare management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${fontSans.variable} `}>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
+    </html>
+  );
+}
