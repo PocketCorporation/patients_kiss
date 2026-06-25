24dde43ca85ea7e4a75e75f4e1bc492fd098ae6f
PocketCorporation <73258409+PocketCorporation@users.noreply.github.com>
2026-06-25 16:56:42 +0000

    Shadcn set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider"
 
-const fontSans = Plus_Jakarta_Sans({
-  variable: "--font-sans",
-  subsets: ["latin"],
-  weight:['300','400','500','600','700']
-});
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: "Patients Kiss",
@@ -20,8 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${fontSans.variable} `}>
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
+    <html lang="en" className={cn("font-sans", geist.variable)} suppressHydrationWarning>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', geist.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-white text-3xl font-bold underline">
+        Hello world!!!
+      </h1>
+      <Button>Click Me</Button>
+      
+    </div>
+
   )
 }
